da325ab099d97fe4847a53286e7154519f8d79c3
tankhanhdao <tankhanhdao@gmail.com>
2022-08-01 10:05:32 -0700

    updated 1 file 1/8/22

--- group_anagram.py
+++ group_anagram.py
@@ -1,6 +1,7 @@
 """Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
 An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once."""
+from re import S
 from typing import List, Tuple
 class Solution:
     # Solution 1: using hashmap to group the string with the common hashkey 
@@ -8,7 +9,6 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         if len(strs) <= 1:
             return [strs]
-        res = []
         hashmap = {}
 
         for st  in strs:
@@ -20,9 +20,7 @@ class Solution:
                 temp_val.append(str)
                 map[key] = temp_val
 
-        for value in map.values():
-            res.append(value)
-        return res
+        return hashmap.values()
 
     def hash(self, st: str) -> Tuple(int):  # O(n) depends on the size of string, O(1) space
         x = tuple()
@@ -31,3 +29,17 @@ class Solution:
             x = y
         return tuple(sorted(x))  # casting into a tuple because we need it to be used as a key in dict
 
+# ===================================================================================
+# Solution 2: using tuple, hashmap - O(len(each string in input) * len(input)) time complexity, O(n) space comp
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = collections.defaultdict(list)
+
+        for st in strs:
+            count = [0] * 26  # since we have 26 letters
+
+            for char in st:    
+                count[ord(char)-ord('a')] += 1  # brilliant way to count the appearance of a letter
+                
+            res[tuple(count)].append(st)
+        
+        return res.values()
