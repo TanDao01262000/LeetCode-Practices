80dc57b9c655415696ba884985673d9f422de93b
tankhanhdao <tankhanhdao@gmail.com>
2022-08-25 17:55:54 -0700

    updated 1 file 08/25/22

--- two_sum_II.py
+++ two_sum_II.py
@@ -16,3 +16,21 @@ class Solution:
             else:
                 break
         return res
+
+
+    # Solution 2: using two pointers, left and right
+    # similar concept of narrow down the range of the binary search 
+    def twoSum(self, numbers: list[int], target:int) -> list[int]:
+        res = []
+        l, r = 0, len(numbers)
+        
+        while l < r:
+            total = numbers[l] + numbers[r]
+            if total < target:
+                l += 1
+            elif total > target:
+                r -= 1
+            else:
+                res.append(l+1)
+                res.append(r+1)
+                return res
